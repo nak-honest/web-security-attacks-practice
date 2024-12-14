9353bab54eca34d1e378ccb6b9a004e4841576b8
nhlee98 <e5slnh9876@gmail.com>
2024-12-14 17:18:13 +0900

    feat: 타임리프에서 XSS 공격 방어

--- xss/src/main/java/com/xss/xss/XssController.java
+++ xss/src/main/java/com/xss/xss/XssController.java
@@ -11,15 +11,24 @@ public class XssController {
     /*
     사용자로 부터 받은 키워드를 그대로 HTML 에 삽입한다.
     다음과 같은 url 접속 시 url에 삽입된 스크립트가 실행된다.
-    http://localhost:8080/xss-reflected?keyword=<script>alert("당신의 컴퓨터는 해킹당했습니다!")</script>
+    http://localhost:8080/xss-reflected-for-attack?keyword=<script>alert("당신의 컴퓨터는 해킹당했습니다!")</script>
 
     로그인 후, 다음 링크로 접속하면 액세스 토큰이 alert 되는 것을 확인할 수 있다.
-    http://localhost:8080/xss-reflected?keyword=<script>alert(decodeURIComponent(document.cookie.split('; ').find(row => row.startsWith('token='))?.split('=')[1]));</script>
+    http://localhost:8080/xss-reflected-for-attack?keyword=<script>alert(decodeURIComponent(document.cookie.split('; ').find(row => row.startsWith('token='))?.split('=')[1]));</script>
      */
-    @GetMapping(path = "/xss-reflected")
-    public String attackXssReflected(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
+    @GetMapping(path = "/xss-reflected-for-attack")
+    public String attackXssReflectedForAttack(Model model,
+                                              @RequestParam(name = "keyword", required = false) String keyword) {
 
         model.addAttribute("keyword", keyword);
-        return "xss-reflected";
+        return "xss-reflected-for-attack";
+    }
+
+    @GetMapping(path = "/xss-reflected-for-defend")
+    public String attackXssReflectedForDefend(Model model,
+                                              @RequestParam(name = "keyword", required = false) String keyword) {
+
+        model.addAttribute("keyword", keyword);
+        return "xss-reflected-for-defend";
     }
 }
